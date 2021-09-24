8682c24d07349ab43764dc1a2e0a1885ee1f7433
SofiaWeber <SofiaWeber@0-209-R10>
2021-09-24 09:13:10 +0200

    in

---
+++ JavaWillNurSpielen/src/TestVertex.java
@@ -0,0 +1,17 @@
+
+public class TestVertex {
+
+	public static void main(String[] args) {
+		
+		Vertex v1 = new Vertex(42, 4) ;
+		Vertex v2 = new Vertex(87, 44) ;
+		Vertex v3 = new Vertex(0, 0) ;
+		
+		System.out.println(v1) ;
+		System.out.println(v2) ;
+		System.out.println(v3) ;
+		
+
+	}
+
+}
--- JavaWillNurSpielen/src/Vertex.java
+++ JavaWillNurSpielen/src/Vertex.java
@@ -1,9 +1,16 @@
 
 public class Vertex {
+	public double x;
+	public double y;
 
-	public static void main(String[] args) {
+	public Vertex(double x, double y) {
+		this. x=x;
+		this. y = y;
+	}
+	
+	public String toSring() {
 		
-
+		return "x = "+x+", y = "+y;
 	}
 
 }
