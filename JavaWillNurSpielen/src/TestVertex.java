3013422edfe9d783ea3608625b2e8d7755ffc2ac
SofiaWeber <SofiaWeber@0-208-07>
2021-09-28 15:27:13 +0200

    initial commit

--- JavaWillNurSpielen/src/TestVertex.java
+++ JavaWillNurSpielen/src/TestVertex.java
@@ -3,14 +3,16 @@ public class TestVertex {
 
 	public static void main(String[] args) {
 		
-		Vertex v1 = new Vertex(42, 4) ;
-		Vertex v2 = new Vertex(87, 44) ;
-		Vertex v3 = new Vertex(0, 0) ;
+		Vertex v1 = new Vertex(10, 5) ;
+		Vertex v2 = new Vertex(27, 25) ;
+		Vertex v4 = v1.skalarMult(42.42) ;
+		Vertex katze = v2.add(v1) ;
 		
 		System.out.println(v1) ;
 		System.out.println(v2) ;
-		System.out.println(v3) ;
-		
+		System.out.println(v2.lenght()) ;
+		System.out.println(v4) ;
+		System.out.println(katze) ;
 
 	}
 
--- JavaWillNurSpielen/src/Vertex.java
+++ JavaWillNurSpielen/src/Vertex.java
@@ -4,13 +4,48 @@ public class Vertex {
 	public double y;
 
 	public Vertex(double x, double y) {
-		this. x=x;
-		this. y = y;
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public String toString() {
+		
+		return "(" + x + ", " + y + ")";
+	}
+
+	public double lenght() {
+		
+		return Math.sqrt(x * x + y * y);
 	}
 	
-	public String toSring() {
+	public Vertex skalarMult(double s) {
 		
-		return "x = "+x+", y = "+y;
+		return new Vertex(x*s, y*s) ;
+		
+	}
+	
+	public void SkalarMultMod(double s) {
+		x = x*s ;
+		y = y*s ;
+	}
+	
+	public Vertex add(Vertex v2) {
+		return new Vertex (x+v2.x , y+v2.y) ;
+	}
+	
+	public void addMod(Vertex hund) {
+		x = x+hund.x ;
+		y = y+hund.x ;
+		
+	}
+	
+	public void setX (double x) {
+		this.x = x ;
+	}
+	
+	public void setY (double y) {
+		this.y = y ;
 	}
 
 }
