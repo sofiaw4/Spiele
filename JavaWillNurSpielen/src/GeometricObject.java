40665b56da5560f6c7f89def128dad8b4b37006d
SofiaWeber <SofiaWeber@0-209-R11>
2021-10-08 09:12:09 +0200

    initial commit

--- JavaWillNurSpielen/src/GeometricObject.java
+++ JavaWillNurSpielen/src/GeometricObject.java
@@ -1,55 +1,88 @@
 
 public class GeometricObject {
-	 
+
 	public Vertex pos;
-	public double height; 
+	public double height;
 	public double width;
 
-	public GeometricObject (double width, double height, Vertex pos) {
+	public GeometricObject(double width, double height, Vertex pos) {
 		this.width = width;
 		this.height = height;
 		this.pos = pos;
-	
-		if (width<0) {
+
+		if (width < 0) {
 			width = -width;
 			pos.x = pos.x - width;
 		}
-	
-		if (height<0) {
+
+		if (height < 0) {
 			height = -height;
-			pos.y = pos.y -height;
-		
+			pos.y = pos.y - height;
+
 		}
-		
+
 	}
-		
-	public GeometricObject (double width, double height) {
-		this(width, height, new Vertex(0,0));
+
+	public GeometricObject(double width, double height) {
+		this(width, height, new Vertex(0, 0));
 	}
-		
-	public GeometricObject (double w) {
-		this(w,w);
+
+	public GeometricObject(double w) {
+		this(w, w);
 	}
-	
+
 	public GeometricObject() {
 		this(10);
 	}
-	
-	public  String toString() {
-		return "width= " +width+", height=" +height+", pos=" +pos;
+
+	public String toString() {
+		return "width= " + width + ", height=" + height + ", pos=" + pos;
 	}
-	
+
 	public double circumference() {
-		return 2*(width+height);
+		return 2 * (width + height);
 	}
-	
-	public double area () {
-		return width*height;
+
+	public double area() {
+		return width * height;
 	}
-	
-	public double getWidth() {return width; }	
-	public double getHeight() {return height; }	
-	public Vertex getPos() {return pos; }
-	
+
+	public double getWidth() {
+		return width;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public Vertex getPos() {
+		return pos;
+	}
+
+	public boolean isLargerThan(GeometricObject that) {
+		return this.area() > that.area();
+	}
+
+	public void moveTo(Vertex pos) {
+		this.pos = pos;
+	}
+
+	public void moveTo(double x, double y) {
+		moveTo(new Vertex(x, y));
+	}
+
+	public void move(Vertex v) {
+		moveTo(pos.add(v));
+	}
+
+	public boolean equals(Object thatObject) {
+		if (thatObject instanceof GeometricObject) {
+			GeometricObject that = (GeometricObject) thatObject;
+			return that.width == this.width 
+					&& this.height == that.height 
+					  && this.pos.equals(that.pos);
+		}
+		return false;
+	}
 
 }
--- JavaWillNurSpielen/src/TestVertex.java
+++ JavaWillNurSpielen/src/TestVertex.java
@@ -3,30 +3,16 @@ public class TestVertex {
 
 	public static void main(String[] args) {
 		
-		String s1 ="hallo" ;
-		String s2 ="hallO".toLowerCase() ;
+		GeometricObject o1 = new GeometricObject(17,4, new Vertex (42, 23));
+		GeometricObject o2 = new GeometricObject (17,4);
+		GeometricObject o3 = new GeometricObject (17);
+		GeometricObject o4 = new GeometricObject ();
 		
-		Vertex v1 = new Vertex(17, 4) ;
-		Vertex v2 = new Vertex(42, 23) ;
-		Vertex v3 = new Vertex(0, 0) ;
-		Vertex v4 = new Vertex(42, 23) ;
-		
-		System.out.println(v2.toString());
-		System.out.println(v2) ;
-		System.out.println(v1.lenght()) ;
-		System.out.println(v2.lenght()) ;
-		System.out.println(v3.lenght()) ;
-		System.out.println(v1.skalarMult(0.5)) ;
-		System.out.println(v1) ;
-		System.out.println(v1.add (v2)) ;
-		System.out.println(v1) ;
-		System.out.println(v2) ;
-		System.out.println(v1.equals(v2)) ;
-		System.out.println(v2.equals(v3)) ;
-		System.out.println(v2.equals(v4)) ;
-		System.out.println(v4.equals(v2)) ;
-		System.out.println(v1.distance(v2)) ;
-		
+		System.out.println(o1.equals(o2));
+		o2.moveTo(42,23);
+		System.out.println(o1.equals(o2));
+		//System.out.println (o3.contains (new Vertex (10,3)));
+		System.out.println(o4);
 	}
 
 }
