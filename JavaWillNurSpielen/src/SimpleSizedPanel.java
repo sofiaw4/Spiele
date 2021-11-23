39b13c98468c5a33ea210b451ddb11fa1bbe7d98
SofiaWeber <SofiaWeber@0-208-07>
2021-11-23 15:30:15 +0100

    in

--- JavaWillNurSpielen/src/SimpleSizedPanel.java
+++ JavaWillNurSpielen/src/SimpleSizedPanel.java
@@ -4,7 +4,7 @@ import util.ShowInFrame;
 public class SimpleSizedPanel extends SizedPanel {
 	
 	public void paintComponent(java.awt.Graphics g) {
-		g.fillRect(30,  50, 45, 80);
+		g.fillRect(30,  50, 50, 80);
 	}
 
 	public static void main(String[] args) {
