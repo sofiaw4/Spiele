a3c635303e5cbdde13a41b719ad71a43206cedca
SofiaWeber <SofiaWeber@0-208-07>
2021-11-23 15:23:12 +0100

    initial commit

---
+++ JavaWillNurSpielen/src/SimpleSizedPanel.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import util.ShowInFrame;
+
+public class SimpleSizedPanel extends SizedPanel {
+	
+	public void paintComponent(java.awt.Graphics g) {
+		g.fillRect(30,  50, 45, 80);
+	}
+
+	public static void main(String[] args) {
+		ShowInFrame.show(new SimpleSizedPanel());
+
+	}
+
+}
---
+++ JavaWillNurSpielen/src/SizedPanel.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+
+import util.ShowInFrame;
+
+import java.awt.Dimension;
+
+public class SizedPanel extends JPanel {
+	int preferredWidth = 400;
+	int preferredHeight = 300;
+	
+	public SizedPanel(int w, int h) {
+		preferredWidth = w;
+		preferredHeight = h;
+	}
+	
+	public SizedPanel() { }
+	
+	@Override public Dimension getPreferredSize() {
+		return new Dimension(preferredWidth, preferredHeight);
+	}
+	
+public static void main(String[] args) {
+	ShowInFrame.show(new SizedPanel());
+}
+
+}
