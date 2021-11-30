69b203de321eea4563ec99a5a7aee43df18ba216
SofiaWeber <SofiaWeber@0-208-07>
2021-11-30 15:30:59 +0100

    initial commit

---
+++ JavaWillNurSpielen/src/PaintableArrayPanel.java
@@ -0,0 +1,19 @@
+
+public class PaintableArrayPanel extends SizedPanel{
+	Paintable[] ps;
+	
+	public PaintableArrayPanel(Paintable[] pa) {
+		ps = pa;
+	}
+	
+	public void paintComponent(java.awt.Graphics g) {
+		for(Paintable p:ps)
+			p.paintTo(g);
+	}
+	
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
